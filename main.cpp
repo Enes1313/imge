05dc56ee0563ca48d63056de895017b9e53d956e
Enes aydın <destansi.13@gmail.com>
2020-12-05 00:44:27 +0300

    Deneme için bir dosya yüklendi ve full ekran açılış

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include "imge.h"
 
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QString>
+#include <QDebug>
+#include <QFileInfo>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Imge w;
-    w.show();
-    return a.exec();
+    QCommandLineParser commandLineParser;
+    commandLineParser.addHelpOption();
+    commandLineParser.addPositionalArgument(Imge::tr("[file]"), Imge::tr("Image file to open."));
+    commandLineParser.process(QCoreApplication::arguments());
+    QFileInfo file{commandLineParser.positionalArguments().front()};
+
+    if (!commandLineParser.positionalArguments().isEmpty() && file.isFile()) {
+        Imge w;
+        QGuiApplication::setApplicationDisplayName(Imge::tr(file.fileName().toStdString().c_str()));
+        w.setWindowState(Qt::WindowFullScreen);
+        w.show();
+        a.exec();
+    }
+    return 0;
 }
